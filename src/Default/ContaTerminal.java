eab8c9eb2c0914362edc3202cddafaa109f1bd0a
IsraelAbreu <israeloliveira18@hotmail.com>
2025-03-07 16:37:09 -0300

    Feat: primeira versão do projeto sem validações

--- src/Default/ContaTerminal.java
+++ src/Default/ContaTerminal.java
@@ -1,5 +1,38 @@
 package Default;
 
 public class ContaTerminal {
-
+	private int numero;
+	private final String agencia = "1010";
+	private String nomeCliente;
+	private double saldo;
+	
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public String getAgencia() {
+		return agencia;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
 }
--- src/Default/Main.java
+++ src/Default/Main.java
@@ -1,10 +1,34 @@
 package Default;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		var scan = new Scanner(System.in);
+		var conta = new ContaTerminal();		
+		
+		System.out.println("Olá, seja vem vindo ao agente de criação de conta");
+		System.out.println("Agência de criação: " + conta.getAgencia());
+		System.out.println("=======================");
+		System.out.println("Por gentileza informe o número da conta:");
+		var numero = scan.nextInt();
+		System.out.println("Informe o nome do cliente:");
+		var nome = scan.next();
+		System.out.println("Informe o valor de depósito de abertura:");
+		var saldo = scan.nextDouble();
+		
+		
+		
+		conta.setNumero(numero);
+		conta.setNomeCliente(nome);
+		conta.setSaldo(saldo);
+		
+		System.out.println("#### Conta criada com sucesso ####");
+		System.out.printf("Número: %s - Agência: %s\n", conta.getNumero(), conta.getAgencia());
+		System.out.printf("Cliente: %s - Saldo: R$ %s\n", conta.getNomeCliente(), conta.getSaldo());
+		
 	}
 
 }
